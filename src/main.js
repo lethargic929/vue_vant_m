5335db2ed61327bab10f348ab6ffc8e2add80cff
lethargic929 <429815654@qq.com>
2020-07-05 14:35:56 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入全局样式
+import '@/styles/index.less'
+// 引入vant和样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 加载动态设置rem的基准值
+import 'amfe-flexible'
+
+// Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
